3902b086ac99991172d14662dad73674e2c72cbf
React Learning <amwilson2019@gmail.com>
2019-07-12 13:01:24 -0400

    Got the header done, working on tabs

--- components/Header/index.js
+++ components/Header/index.js
@@ -8,5 +8,25 @@
 //    <span class="temp">98°</span>
 //  </div >
 // And add it to the DOM in the .headerContainer component
-
-function Header() {}
+const headerContainer = document.querySelector('.header-container');
+function Header() {
+    //Creating the HTML elements
+    const header = document.createElement('div');
+    const date = document.createElement('span');
+    const lambdaTime = document.createElement('h1');
+    const temp = document.createElement('span');
+    //Apend elements
+    header.appendChild(date);
+    header.appendChild(lambdaTime);
+    header.appendChild(temp);
+    //Creating the classList
+    header.classList.add('header');
+    date.classList.add('date');
+    temp.classList.add('temp');
+    //Adding content
+    lambdaTime.textContent = 'Lambda Time';
+    date.textContent = 'March 28, 2019';
+    temp.textContent = '98°';
+    return header;
+}
+headerContainer.appendChild(Header());
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -1,9 +1,41 @@
 // Step 2: Create Tabs
 // -----------------------
-// Using axios send a GET request to the address: https://lambda-times-backend.herokuapp.com/topics
+// Using axios send a GET request to the address: https://lambda-times-backend.herokuapp.com/topics  https://lambda-times-backend.herokuapp.com/articles
 // Once the data is returned console.log it and review the structure.
 // Iterate over the topics creating a new Tab component and add it to the DOM
 // under the .topics element.
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+/* <div class="topics">
+
+      <div class="tab active-tab">ALL</div>
+      <div class="tab">JAVASCRIPT</div>
+      <div class="tab">TECHNOLOGY</div>
+      <div class="tab">NODE.JS</div>
+      <div class="tab">jQUERY</div>
+      <div class="tab">BOOTSTRAP</div>
+      <!-- My Artricles -->
+      <div data-tab="ajax" class="tab">Ajax</div>
+      <div data-tab="xml" class="tab">XML</div>
+    </div> */
+//Getting data from API
+axios.get(`https://lambda-times-backend.herokuapp.com/topics`)
+.then(getData =>{
+    const pullData = getData.data;
+    console.log(pullData);
+    topics.appendChild(Tabs(getData));
+});
+
+
+const topics = document.querySelector('.topics');
+function Tabs() {
+  const tab = document.createElement('div');
+ 
+  topics.appendChild(tab);
+  
+  tab.classList.add('tab');
+  tab.textContent = 'All';
+  return tab;
+}
+console.log(Tabs())
